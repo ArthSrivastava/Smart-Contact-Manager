dd0247b5cf400a35a010296094b22e341370ae37
Arth <arth4701@gmail.com>
2022-10-24 14:47:41 +0530

    made search controller as a normal controller rather than a rest controller

--- src/main/java/com/theSnoozingTurtle/controller/SearchController.java
+++ src/main/java/com/theSnoozingTurtle/controller/SearchController.java
@@ -6,14 +6,14 @@ import com.theSnoozingTurtle.entities.Contact;
 import com.theSnoozingTurtle.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.security.Principal;
 import java.util.List;
 
-@RestController
+@Controller
 public class SearchController {
     @Autowired
     private UserRepository userRepository;
